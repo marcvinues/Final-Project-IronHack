f89ba1e85ee14eb8e1c40903d8ebffef052a38bf
Marc Vinues <info@marcvinues.com>
2016-05-17 15:41:36 +0200

    markers

--- app/assets/javascripts/maps/gmaps.js
+++ app/assets/javascripts/maps/gmaps.js
@@ -10,7 +10,7 @@ function gmaps_init(){
   var latlng = new google.maps.LatLng(41.3865828,2.1609558);
 
   var options = {
-    zoom: 14,
+    zoom: 13,
     center: latlng,
     scrollwheel: false,
     mapTypeId: google.maps.MapTypeId.ROADMAP
@@ -28,7 +28,29 @@ function gmaps_init(){
     draggable: true
   });
 
-
+  var contentString = '<div id="content">'+
+        '<div id="siteNotice">'+
+        '</div>'+
+        '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+        '<div id="bodyContent">'+
+        '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+        'sandstone rock formation in the southern part of the '+
+        'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+        'south west of the nearest large town, Alice Springs; 450&#160;km '+
+        '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+        'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+        'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+        'Aboriginal people of the area. It has many springs, waterholes, '+
+        'rock caves and ancient paintings. Uluru is listed as a World '+
+        'Heritage Site.</p>'+
+        '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+        'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+        '(last visited June 22, 2009).</p>'+
+        '</div>'+
+        '</div>';
+        var infowindow = new google.maps.InfoWindow({
+           content: contentString
+         });
 
   // event triggered when marker is dragged and dropped
   google.maps.event.addListener(marker, 'dragend', function() {
@@ -39,11 +61,11 @@ function gmaps_init(){
   google.maps.event.addListener(map, 'click', function(event) {
     marker.setPosition(event.latLng)
     geocode_lookup( 'latLng', event.latLng  );
+    infowindow.open(map, marker);
   });
 
   $('#gmaps-error').hide();
 }
-
 
 
 
@@ -173,9 +195,42 @@ function showPrice(){
  });
     bermudaTriangle.setMap(map);
 };
-$('#btnprice').click(function () {
-    bermudaTriangle.setVisible(null);
-});
+
+function showPrice(){
+  var coords =[
+    {lat:41.378134, lng:2.157709},
+    {lat:41.383165, lng:2.150757},
+    {lat:41.387069, lng:2.158160},
+    {lat:41.378134, lng:2.162580}
+  ];
+  var price = new google.maps.Polygon({
+   paths: coords,
+   strokeColor: '#FF0000',
+   strokeOpacity: 0.8,
+   strokeWeight: 2,
+   fillColor: '#FF0000',
+   fillOpacity: 0.35
+ });
+    price.setMap(map);
+};
+
+function showQuiet(){
+  var coords =[
+    {lat:41.399154, lng:2.130923},
+    {lat:41.406340, lng:2.123745},
+    {lat:41.410605, lng:2.129442},
+    {lat:41.404184, lng:2.136738}
+  ];
+  var quiet = new google.maps.Polygon({
+   paths: coords,
+   strokeColor: '#FF0000',
+   strokeOpacity: 0.8,
+   strokeWeight: 2,
+   fillColor: '#FF0000',
+   fillOpacity: 0.35
+ });
+    quiet.setMap(map);
+};
 
 
 
